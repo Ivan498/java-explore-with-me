f25bd46fbe4edf6429e90d3081867ba20dee408f
Ivan498 <vankar2824@mail.ru>
2024-09-11 23:07:25 +0300

    ошибка

--- ewm-stats-service/stats-service/src/main/java/ru/practicum/stats/service/hit/repository/HitRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.stats.service.hit.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.practicum.stats.service.hit.model.Hit;
-
-public interface HitRepository extends CrudRepository<Hit, Long>, CustomizedHitRepository {
-}
