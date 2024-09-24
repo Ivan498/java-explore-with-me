04f2aceecbdf9291a5c0014322b89746b83afe60
Ivan498 <vankar2824@mail.ru>
2024-09-24 11:20:24 +0300

    fix 3

--- ewm-main-service/src/main/java/ru/practicum/ewm/comment/controller/PrivateCommentController.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/comment/controller/PrivateCommentController.java
@@ -9,6 +9,8 @@ import ru.practicum.ewm.comment.dto.CommentDto;
 import ru.practicum.ewm.comment.dto.CommentShortDto;
 import ru.practicum.ewm.comment.dto.NewCommentDto;
 import ru.practicum.ewm.comment.service.CommentService;
+import ru.practicum.ewm.exception.NotFoundException;
+import ru.practicum.ewm.exception.PermissionDeniedException;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
@@ -29,7 +31,7 @@ public class PrivateCommentController {
     public CommentDto postComment(@PathVariable Long userId,
                                   @PathVariable Long eventId,
                                   @Valid @RequestBody NewCommentDto commentDto) {
-        log.info("Получен запрос на добавления комментария к событию id={} от пользователя id={}: {}",
+        log.debug("Получен запрос на добавление комментария к событию id={} от пользователя id={}: {}",
                 eventId, userId, commentDto.toString());
         return commentService.addComment(userId, eventId, commentDto);
     }
@@ -38,7 +40,7 @@ public class PrivateCommentController {
     public List<CommentShortDto> getCommentsByUser(@PathVariable Long eventId,
                                                    @RequestParam(defaultValue = "0") @PositiveOrZero Integer from,
                                                    @RequestParam(defaultValue = "10") @Positive Integer size) {
-        log.info("Получен запрос на просмотр комментариев к событию id={} from={} size={}", eventId, from, size);
+        log.debug("Получен запрос на просмотр комментариев к событию id={} from={} size={}", eventId, from, size);
         return commentService.getAllCommentsByUser(eventId, from, size);
     }
 
@@ -47,7 +49,7 @@ public class PrivateCommentController {
                                    @PathVariable Long eventId,
                                    @PathVariable Long commentId,
                                    @Valid @RequestBody NewCommentDto commentDto) {
-        log.info("Получен запрос на редактирование комментария id={}, eventId={}, userId={}: {}",
+        log.debug("Получен запрос на редактирование комментария id={}, eventId={}, userId={}: {}",
                 commentId, eventId, userId, commentDto.toString());
         return commentService.updateComment(userId, eventId, commentId, commentDto);
     }
@@ -55,10 +57,23 @@ public class PrivateCommentController {
     @DeleteMapping("/{commentId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteComment(@PathVariable Long userId,
-                                    @PathVariable Long eventId,
-                                    @PathVariable Long commentId) {
-        log.info("Получен запрос на удаление комментария id={} от пользователя id={}", commentId, userId);
+                              @PathVariable Long eventId,
+                              @PathVariable Long commentId) {
+        log.debug("Получен запрос на удаление комментария id={} от пользователя id={}", commentId, userId);
         commentService.deleteComment(userId, eventId, commentId);
     }
 
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleUserNotFound(NotFoundException e) {
+        log.error("Пользователь не найден: {}", e.getMessage());
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(PermissionDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String handleAccessDenied(PermissionDeniedException e) {
+        log.error("Доступ запрещен: {}", e.getMessage());
+        return e.getMessage();
+    }
 }
